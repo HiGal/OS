a8a9ef36b2e10c5c470fa4c88463e2a1b77ba48f
HiGal <f.galeev@innopolis.ru>
2018-11-08 13:46:13 +0300

    added lab 12

--- week12/ex2.c
+++ week12/ex2.c
@@ -1,52 +1,53 @@
+#include <ctype.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
-#include <ctype.h>
+
 
 int main(int argc, char *argv[])
 {
 	
-	//resolve arguments
-	char c;
+	char ch;
 	int append_mode = 0;
-	while ((c = getopt(argc, argv, "a")) != -1) {
+	while ((ch = getopt(argc, argv, "a")) != -1) {
 		switch (c) {
 		case 'a':
 			append_mode = 1;
 			break;
-		case '?':
-			if (isprint(optopt))
-				fprintf(stderr, "Unkonw option `-%c'.\n", optopt);
-			else
-				fprintf(stderr,
-					"Unknown option character `\\x%x'.\n", optopt);
-			return 1;
 		default:
 			abort();
 			break;
 		}
 	}
 	
-	int i;
-	FILE *files[20];
+	
+	
 	int num_files;
-	char buf[100];
+	char buffer[100];
 	size_t len;
 	char *file_mode;
+	FILE *files[20];
+	int i;
+
+	if(append_mode == 1)
+		file_mode = "a";
+	else
+		file_mode = "w";
 	
-	file_mode = (append_mode ? "a" : "w");
 	num_files = argc - optind;
 	
 	if (num_files > 0) {
 		for (i = optind; i < argc; i++) {
 			FILE *pFile = fopen(argv[i], file_mode);
-			files[i - optind] = pFile; /* mind the offset */
+			files[i - optind] = pFile; 
 		}
 	}
 	
-	FILE *not_stdin = fopen("tee.c", "r");
-	while ((len = fread(&buf[0], 1, sizeof(buf), not_stdin)) > 0) {
-		fwrite(&buf[0], 1, len, stdout);
+	FILE *other_stdin = fopen("tee.c", "r");
+	len = fread(&buffer[0], 1, sizeof(buffer),other_stdin)
+	while (len > 0) {
+		len = fread(&buffer[0], 1, sizeof(buffer),other_stdin)
+		fwrite(&buffer[0], 1, len, stdout);
 		for (i = 0; i < num_files; i++) {
 			fwrite(&buf[0], 1, len, files[i]);
 		}
